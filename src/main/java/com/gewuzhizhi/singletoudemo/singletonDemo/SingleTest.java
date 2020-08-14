229c61fd8157c23eb1c1b252c8a01f5ee2ed936e
Boketto <15707951907@139.com>
2020-08-14 16:59:43 +0800

    测试删除

---
+++ src/main/java/com/gewuzhizhi/singletoudemo/singletonDemo/SingleTest.java
@@ -0,0 +1,11 @@
+package com.gewuzhizhi.singletoudemo.singletonDemo;
+
+/**
+ * @ClassName : SingleTest
+ * @Description : 测试删除
+ * @Author :
+ * @Date: 2020-08-14
+ * @Version:
+ */
+public class SingleTest {
+}
